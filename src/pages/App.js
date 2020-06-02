46c08f5d0d4eb72d2c849dfb17b28ed4f5b5e091
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-01 20:23:32 -0700

    nextjs needs an index.js file within pages folder to render correctly

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
 
 const App = () => {
   return (
     <div>
-      <h1>first route, home screen</h1>
+      <ul>
+        {/* <li>
+        <Link href="/" as="/App">
+          <p>Home Page</p>
+        </Link>
+      </li> */}
+        <li>
+          <Link href="/Hello" as="/Hello">
+            <p>Hello Page</p>
+          </Link>
+        </li>
+      </ul>
+      <p>HEllo World</p>
     </div>
   );
 };
