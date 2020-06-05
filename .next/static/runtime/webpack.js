795b3b5e5117f662914f7eaa8f34a04f7706fd0c
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-04 23:07:42 -0700

    adding new page

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b4ad20a43ad3f1d4f9b4";
+/******/ 	var hotCurrentHash = "dca878d47886aefef2d3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
