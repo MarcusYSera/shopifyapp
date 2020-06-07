192e3d3c93609f035e856dc83055de0a169c4aed
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-06 22:04:18 -0700

    added graphql and adding db

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,25 @@ import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
 import { Provider } from '@shopify/app-bridge-react';
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 import '@shopify/polaris/styles.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+    const config = {
+      apiKey: API_KEY,
+      shopOrigin: Cookies.get('shopOrigin'),
+      forceRedirect: true,
+    };
 
     return (
       <React.Fragment>
@@ -17,9 +29,9 @@ class MyApp extends App {
           <meta charSet="utf-8" />
         </Head>
         <Provider config={config}>
-          <AppProvider>
+          <ApolloProvider client={client}>
             <Component {...pageProps} />
-          </AppProvider>
+          </ApolloProvider>
         </Provider>
       </React.Fragment>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import store from 'store-js';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
