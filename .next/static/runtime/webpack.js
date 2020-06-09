16fefec8e31a225c4632e29ef38a5b24c82ad888
Marcus Sera <m.yoshiosera@gmail.com>
2020-06-08 23:28:03 -0700

    working out store list

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8afc04ceef69e22396a6";
+/******/ 	var hotCurrentHash = "19a758ffe14c7985ef88";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
